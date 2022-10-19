ba217002ee0d3a571a4d2f880e2cee8a2e622e79
MaxDePro <xmaksg@gmail.com>
2022-10-19 16:03:42 +0300

    first commit

---
+++ events/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+import calendar
+from calendar import HTMLCalendar
+from datetime import datetime
+
+# Create your views here.
+
+
+def home(request, year=datetime.now().year, month=datetime.now().strftime('%B')):
+    month = month.capitalize()
+    month_number = list(calendar.month_name).index(month)
+    month_number = int(month_number)
+    name = 'John'
+
+    now = datetime.now()
+    cur_year = now.year
+    cur_time = now.strftime('%I:%M %p')
+
+    cal = HTMLCalendar().formatmonth(year, month_number)
+
+    context = {
+        'name': name,
+        'year': year,
+        'month': month,
+        'month_number': month_number,
+        'cal': cal,
+        'cur_year': cur_year,
+        'cur_time': cur_time,
+    }
+    return render(request, 'events/home.html', context)
